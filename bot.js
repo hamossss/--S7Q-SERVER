b906180c1f57cd58282255335b01c370f3be8a44
hamossss <40631448+hamossss@users.noreply.github.com>
2018-09-17 20:04:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -235,40 +235,6 @@ if(!message.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return m
 
 
 
- client.on('message', message => {
-        if(!message.channel.guild) return;
-        var prefix = "ا";
-    if(message.content.startsWith(prefix + 'سحب')) {
-        var cmdrole = message.guild.roles.find("name", config.cmdrole)
-           if (message.member.hasPermission("MOVE_MEMBERS")) {
-                  if (message.mentions.users.size === 0) {
-                         return message.channel.send("``لاستخدام الأمر اكتب هذه الأمر : " +prefix+ "move [USER]``")
-                  }
-                  if (message.member.voiceChannel != null) {
-                         if (message.mentions.members.first().voiceChannel != null) {
-                                var authorchannel = message.member.voiceChannelID;
-                                var usermentioned = message.mentions.members.first().id;
-                               var embed = new Discord.RichEmbed()
-                                  .setTitle("Succes!")
-                                  .setColor("#000000")
-                                  .setDescription(`لقد قمت بسحب <@${usermentioned}> الى الروم الصوتي الخاص بك:white_check_mark: `)
-                                var embed = new Discord.RichEmbed()
-                                  .setTitle(`You are Moved in ${message.guild.name}`)
-                                  .setColor("#000000")
-                                  .setDescription(`<@${message.author.id}> moved you to his channel!\nServer => ${message.guild.name}`)
-                                                              message.guild.members.get(usermentioned).setVoiceChannel(authorchannel).then(m => message.channel.send(embed))
-                                message.guild.members.get(usermentioned).send(embed)
-                         } else {
-                                message.channel.send("``لا تستطيع سحب "+ message.mentions.members.first() +" `يجب ان يكون هذه العضو في روم صوتي`")
-                         }
-                  } else {
-                         message.channel.send("``يجب ان تكون في روم صوتي لكي تقوم بسحب العضو أليك``")
-                  }
-           } else {
-                  message.react("❌")
-           }
-        }
-        });
 
 
 
